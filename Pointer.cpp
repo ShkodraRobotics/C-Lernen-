89158e3f5fed1f87445f6b605a4a2101f33f4959
lolos <lorikshkodra94@gmail.com>
2023-04-24 13:15:44 +0200

    cplusplus

--- Pointer.cpp
+++ Pointer.cpp
@@ -8,5 +8,37 @@ int main()
     intPointer = &num;
 
     std::cout << num << std::endl;
-    std::cout << intPointer;
+    std::cout << intPointer << std::endl;
+
+    std::string *stringPointer = nullptr;
+    std::string strPoint = "Hallo welt ";
+
+    stringPointer = &strPoint;
+    std::string &ref = strPoint;
+
+    std::cout << "stringPointer Addy " << stringPointer << std::endl;
+    std::cout << "stringPointer Wert " << *stringPointer;
+
+    //------------------Intialiesierung----------------------------------------1
+    int *ersterPoint = nullptr;
+    double *dobPinter = nullptr;
+    std::string *strPointer2 = nullptr;
+    //_________________________________________________________2
+    int pNum = 100;
+    double dNum = 100.99;
+    std::string strST = "Pointer ubung ";
+
+    //_______________Pointer referenz__________________________________________2.5
+    ersterPoint = &pNum;
+    dobPinter = &dNum;
+    strPointer2 = &strST;
+
+    //________________________Speicher adresse_________________________________3
+    std::cout << "Int pointer addresse: " << ersterPoint << std::endl;
+    std::cout << "double pointer addresse: " << dobPinter << std::endl;
+    std::cout << "string Pointer addresse: " << strPointer2 << std::endl;
+    //__________________________Wert_______________________________4
+    std::cout << "int pointer wert = " << *ersterPoint << std::endl;
+    std::cout << "int pointer wert = " << *dobPinter << std::endl;
+    std::cout << "int pointer wert = " << *strPointer2 << std::endl;
 }
