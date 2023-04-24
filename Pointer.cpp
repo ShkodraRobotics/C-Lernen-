4563f2dbe47f0d1f34ef11949d97494038bf2007
lolos <lorikshkodra94@gmail.com>
2023-04-24 14:41:39 +0200

    Übungen

--- Pointer.cpp
+++ Pointer.cpp
@@ -11,10 +11,10 @@ int main()
     int *intPointer = nullptr;
     int num = 10;
     intPointer = &num;
-
-    std::cout << num << std::endl;
-    std::cout << intPointer << std::endl;
-
+    /*
+        std::cout << num << std::endl;
+        std::cout << intPointer << std::endl;
+    */
     std::string *stringPointer = nullptr;
     std::string strPoint = "Hallo welt ";
 
@@ -25,19 +25,27 @@ int main()
         std::cout << "stringPointer Wert " << *stringPointer;
     */
     //------------------Intialiesierung----------------------------------------1
-    int *ersterPoint = nullptr;
-    double *dobPinter = nullptr;
-    std::string *strPointer2 = nullptr;
+    int *ersterPoint = nullptr;         //
+    double *dobPinter = nullptr;        //
+    std::string *strPointer2 = nullptr; //
+    float *fPointer = nullptr;
+    int *Pointerübung1 = nullptr;
+    int *PointerÜbung2 = nullptr;
+    double *PointerÜbung3 = nullptr;
 
     //_________________________________________________________2
     int pNum = 100;
     double dNum = 100.99;
     std::string strST = "Pointer ubung ";
-
+    float fNum = 0.99;
     //________________________Pointer referenz__________________________________________2.5
     ersterPoint = &pNum;
     dobPinter = &dNum;
     strPointer2 = &strST;
+    fPointer = &fNum;
+    Pointerübung1 = &pNum;
+    PointerÜbung2 = &pNum;
+    PointerÜbung3 = &dNum;
     /*
         //________________________Speicher adresse_________________________________3
         std::cout << "Int pointer addresse: " << ersterPoint << std::endl;
@@ -49,5 +57,24 @@ int main()
         std::cout << "int pointer wert = " << *strPointer2 << std::endl;
     */
 
-    pointerausgabe(ersterPoint);
+    std::cout << Pointerübung1 << std::endl;
+    std::cout << PointerÜbung2 << std::endl;
+    std::cout << PointerÜbung3 << std::endl;
+
+    // pointerausgabe(ersterPoint);
+
+    //________________________References______________________________
+
+    int a = 10;
+    int &aRef = a;
+    double b = 1.99;
+    double &bref = b;
+    int c = 20;
+    int &cref = c;
+
+    std::cout << a << std::endl;
+    aRef++;
+    std::cout << aRef << std::endl;
+    bref++;
+    std::cout << bref << std::endl;
 }
